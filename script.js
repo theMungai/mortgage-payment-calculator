0e8eb93a22902aa07e0e07f4d1bf9c5f78e0de89
theMungai <mungaithesimon@gmail.com>
2024-11-25 15:00:12 +0300

    Repayment Calculations

--- script.js
+++ script.js
@@ -17,38 +17,6 @@ calculateButton.addEventListener("click", () => {
     resultsCalculation.style.display = "block";
     emptyResults.style.display = "none";
 
-    const calculationHTML = `
-        <div class="your-results">
-            <h3>Your results</h3>
-            <p>Your results are shown below based on the information you provided. 
-                To adjust the results, edit the form and click “calculate repayments” again.
-            </p>
-        </div>
-
-        <div class="calculation">
-            <div class="monthly-repayments">
-                <p>Your monthly repayments</p>
-                <h1 class = "js-monthly-payment">£ ${parseFloat(
-    (mortgageAmount.value * (interestRate.value / 1200) * (1 + (interestRate.value / 1200)) ** (mortgageTerm.value * 12)) / 
-    ((1 + interestRate.value / 1200 ) ** (mortgageTerm.value * 12) - 1)).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</h1>
-
-
-
-            </div>
-
-            <hr>
-            <div class="total-repayments">
-                <p>Total you'll repay over the term</p>
-                <h3 class = "js-total-payment">£ ${parseFloat(
-    ((mortgageAmount.value * (interestRate.value / 1200) * (1 + (interestRate.value / 1200)) ** (mortgageTerm.value * 12)) / 
-    ((1 + interestRate.value / 1200 ) ** (mortgageTerm.value * 12) - 1)) * mortgageTerm.value * 12).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</h3>
-
-            </div>
-            
-        </div>
-`;
-    resultsCalculation.innerHTML = calculationHTML
-
     const amountSpan = document.querySelector(".amount-span");
     const rateSpan = document.querySelector(".rate-span");
     const termSpan = document.querySelector(".term-span");
@@ -73,7 +41,7 @@ calculateButton.addEventListener("click", () => {
         showError("interest-rate-error","This field is required");
         rateSpan.style.color = "white";
         rateSpan.style.backgroundColor = "hsl(4, 69%, 50%)";
-        interestRate.style.borderColor ="hsl(4, 69%, 50%)"
+        interestRate.style.borderColor ="hsl(4, 69%, 50%)";
     }
 
     if(!repaymentRadio.checked && !interestRadio.checked){
@@ -82,6 +50,29 @@ calculateButton.addEventListener("click", () => {
 
     
 });
+
+// Calculations if user clicks repayment div
+const totalPayments = document.querySelector(".js-total-payment")
+const monthlyPayment = document.querySelector(".js-monthly-payment");
+repaymentDiv.addEventListener("click", () => {
+
+    monthlyPayment.innerHTML = parseFloat(
+    (mortgageAmount.value * (interestRate.value / 1200) * (1 + (interestRate.value / 1200)) ** (mortgageTerm.value * 12)) / 
+    ((1 + interestRate.value / 1200 ) ** (mortgageTerm.value * 12) - 1)).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+    totalPayments.innerHTML = parseFloat(
+        ((mortgageAmount.value * (interestRate.value / 1200) * (1 + (interestRate.value / 1200)) ** (mortgageTerm.value * 12)) / 
+        ((1 + interestRate.value / 1200 ) ** (mortgageTerm.value * 12) - 1)) * mortgageTerm.value * 12).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+});
+
+interestOnlyDiv.addEventListener("click", () => {
+    monthlyPayment.innerHTML = "Stick"
+})
+
+
+
+
+
 
 //  Checking Radio Boxes when divs are clicked
 
